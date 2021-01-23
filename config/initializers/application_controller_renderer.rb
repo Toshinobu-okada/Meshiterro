6db5780b8c9bece04cc9cfbc6aa7d184fbf93909
Toshinobu-okada <okacchi_nobu@yahoo.co.jp>
2021-01-23 06:38:37 +0000

    [uproad]コメント機能追加

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = '7c3dc7a8cbc22a7524ba1cfe4cfdd2b179931287a2cdc79ede97670800ffadec05ce963e862e3f791f6b6e973b8d254bb27e00bed39399d111cde643af59a77e'
